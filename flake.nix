77dfea0ae710cdaa13ccae1aa9302e87f37baec2
Devon Mizelle <devon.mizelle@onepeloton.com>
2022-05-23 18:28:25 -0400

    dont need a stdenv with cc

--- flake.nix
+++ flake.nix
@@ -14,41 +14,35 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        # by defining our own texlive distribution, we can trim
+        # down the size of the nix store
         latex = (pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-small
-            svg
-            trimspaces
-            catchfile
-            transparent
-            moresize
-            hyphenat
-            raleway
-            fontawesome5
-            pagecolor
-          ;
+          inherit (pkgs.texlive)
+            scheme-small svg trimspaces catchfile transparent moresize hyphenat
+            raleway fontawesome5 pagecolor;
         });
-        buildInputs = with pkgs; [
-          latex
-          font-awesome
-          fontconfig
-        ];
-        fonts = pkgs.makeFontsConf {
-          fontDirectories = [
-            pkgs.font-awesome
-          ];
+
+        buildInputs = with pkgs; [ latex font-awesome fontconfig ];
+
+        fonts = pkgs.makeFontsConf { fontDirectories = [ pkgs.font-awesome ]; };
+
+        # I would prefer to use a flake input here, but it takes quite a bit to clone this repo.
+        nixLogo = pkgs.fetchurl {
+          url =
+            "https://github.com/NixOS/nixos-artwork/raw/a86492b5338813910e4505b30a8600be2c2a1f4c/logo/white.png";
+          sha256 = "0pd45ya86x1z00fb67aqhmmvm7pk50awkmw3bigmhhiwd4lv9n6h";
         };
-          # I would prefer to use a flake input here, but it takes quite a bit to clone this repo.
-          nixLogo = pkgs.fetchurl {
-            url = "https://github.com/NixOS/nixos-artwork/raw/a86492b5338813910e4505b30a8600be2c2a1f4c/logo/white.png";
-            sha256 = "0pd45ya86x1z00fb67aqhmmvm7pk50awkmw3bigmhhiwd4lv9n6h";
-          };
+
       in {
-        devShell = pkgs.mkShell {
+        devShell = pkgs.mkShell.override {
+          stdenv = pkgs.stdenvNoCC;
+        } {
           buildInputs = buildInputs;
           shellHook = "export FONTCONFIG_FILE=${fonts}";
         };
 
-        defaultPackage = pkgs.stdenv.mkDerivation {
+        # we dont need a compiler here, its just latex! :)
+        defaultPackage = pkgs.stdenvNoCC.mkDerivation {
           name = "resume";
           nativeBuildInputs = buildInputs;
           src = ./.;
